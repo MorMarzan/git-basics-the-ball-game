03e8145f934a342fb266eb8b64dd44bacbdb5d7e
Mor Marzan <mormarzan@gmail.com>
2023-11-15 16:25:27 +0200

    Implement onBallClick()

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict'
 
+var gBallSize = 100
 
-function onBallClick(el) {
-    console.log('elClicked', el)
+function onBallClick(elBall) {
+    gBallSize += 50
+
+    elBall.style.width = gBallSize + 'px'
+    elBall.style.height = gBallSize + 'px'
+
+    elBall.innerText = gBallSize
 }
